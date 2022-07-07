a6b5fd62a2d786ef741a066ae1f06c4bc9ad7ff3
Marcell Magyar <marcell.magyar@netcetera.com>
2022-07-07 14:27:57 +0200

    [NCA421WSRT-6028] REMOVE PMKUIKit + update dependencies' versions

--- Package.swift
+++ Package.swift
@@ -14,16 +14,15 @@ let package = Package(
             targets: ["GirdersSwift", "GRSecurity"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", exact: "1.7.0"),
-        .package(url: "git@github.com:marci0907/PMKUIKit.git", exact: "1.20.0"),
-//        .package(name: "PMKUIKit", path: "Sources/PMKUIKit"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", exact: "3.2.0"),
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", exact: "9.0.0"),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", exact: "1.9.5"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", exact: "6.17.1"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", exact: "4.2.2"),
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", exact: "9.0.0")
     ],
     targets: [
         .target(
             name: "GirdersSwift",
-            dependencies: ["SwiftyBeaver", "PMKUIKit", "KeychainAccess"]),
+            dependencies: ["SwiftyBeaver", "PromiseKit", "KeychainAccess"]),
         .testTarget(
             name: "GirdersSwiftTests",
             dependencies: [
